f30e0b82cbd2080c5c3c3360809e85e02c1e1739
policma1 <matespolicka@seznam.cz>
2022-09-26 10:22:25 +0200

    Added PizzaMenu class and PizzaPolozky class

--- src/models/PizzaMenu.java
+++ src/models/PizzaMenu.java
@@ -22,15 +22,33 @@ public class PizzaMenu {
         lastUpdate = LocalDateTime.now();
     }
 
-    public void removeItem(String name) {
-
+    public void removeItem(String itemToRemove) {
+        PizzaPolozky toDelete = null;
+        for(PizzaPolozky it : menuItems){
+            if(it.getName() == itemToRemove){
+                toDelete = it;
+                break;
+            }
+        }
+        if(toDelete!=null){
+            menuItems.remove(toDelete);
+        }
     }
 
     public List<PizzaPolozky> getMenuItems() {
         return menuItems;
     }
 
+    public int getMenuSize(){
+        return menuItems.size();
+    }
+
     public LocalDateTime getLastUpdate() {
         return lastUpdate;
+    }
+
+    @Override
+    public boolean equals(Object obj){
+        return ((PizzaMenu)obj).getMenuSize() == getMenuSize();
     }
 }
